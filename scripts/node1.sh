810ac90b18de5ce193ba76b0c690e141afd04b9e
MarilenaJouan <112272325+MarilenaJouan@users.noreply.github.com>
2022-09-01 20:46:34 +0300

    Update node1.sh

--- scripts/node1.sh
+++ scripts/node1.sh
@@ -1,13 +1,16 @@
-#!/bin/bash 
+#!/bin/bash
 
-CPU_CONTEXT_SWITCH=$(vmstat -s | grep 'CPU' | awk '{print $1}') 
+FILE=/home/node1/node1-shared/node1.txt
+CPU_CONTEXT_SWITCH=$(vmstat -s | grep 'CPU' | awk '{print $1}')
 
-DATE=$(date +"%x")                                                                                                   
-TIME=$(date +"%T") 
+DATE=$(date +"%x")
+TIME=$(date +"%T")
 
-TIMESTAMP="$DATE-$TIME" 
+TIMESTAMP="$DATE-$TIME"
 
-touch node1.txt                                                                                                      
-chmod +x node1.txt
+if [ ! -f "$FILE" ]; then
+    touch "$FILE"
+    chmod +x "$FILE"
+fi
 
-echo "CPU context switches: $CPU_CONTEXT_SWITCH, timestamp: $TIMESTAMP" 
+echo "CPU context switches: $CPU_CONTEXT_SWITCH, timestamp: $TIMESTAMP"
